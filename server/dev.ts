92261b5edd6364fd0585c9b303b9e822a6cbd7d0
JounQin <admin@1stg.me>
2018-08-27 15:42:55 +0800

    Use Docker for development also (#1)

--- server/dev.ts
+++ server/dev.ts
@@ -1,7 +1,15 @@
 import koaWebpack from 'koa-webpack'
 
+import { serverHost } from '../build/config'
 import config from '../build/webpack.client'
 
 export default koaWebpack({
   config,
+  hotClient: {
+    host: {
+      client: '*',
+      server: serverHost,
+    },
+    port: 7001,
+  },
 })
